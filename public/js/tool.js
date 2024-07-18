7def0030cb732457af8febf8fb5fb0b0a54f1d4a
mohit shah <mohitshah829@gmail.com>
2024-07-18 20:43:28 +0530

    second commit

--- public/js/tool.js
+++ public/js/tool.js
@@ -1,5 +1,5 @@
 // connect to socket server
-const socket = io.connect("https://pepboard-1.onrender.com");
+const socket = io.connect("https://realtime-white-board-6cxt.onrender.com");
 // *********************************Basic Setup
 const board = document.querySelector(".board");
 board.height = window.innerHeight;
